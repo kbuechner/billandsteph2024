6930787badda701e49bd2ca52f760b3d312510d6
Katherine Buechner <Katherine.Buechner@vista.com>
2024-06-21 11:22:41 -0400

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Tabs, Tab, TabList, TabPanels, TabPanel } from '@chakra-ui/react';
+import './App.css';
+import Schedule from './Content/Schedule';
+import Hotels from './Content/Hotels';
+import DressCode from './Content/DressCode';
+import Faq from './Content/Faq';
+import Registry from './Content/Registry';
+import LocationAndParking from './Content/LocationAndParking';
+import Rsvp from './Content/Rsvp';
+import Story from './Content/Story';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box width={'100%'} height={'100%'} backgroundColor={'teal.700'}>
+      <Tabs variant={'enclosed-colored'} isFitted>
+        <TabList>
+          <Tab value='rsvp'>RSVP</Tab>
+          <Tab value='story'>Our Story</Tab>
+          <Tab value='location'>Location &amp; Parking</Tab>
+          <Tab value='schedule'>Schedule</Tab>
+          <Tab value='hotels'>Hotels</Tab>
+          <Tab value='dress'>Dress Code</Tab>
+          <Tab value='faq'>FAQ</Tab>
+          <Tab value='registry'>Registry</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel><Rsvp/></TabPanel>
+          <TabPanel><Story/></TabPanel>
+          <TabPanel><LocationAndParking/></TabPanel>
+          <TabPanel><Schedule/></TabPanel>
+          <TabPanel><Hotels/></TabPanel>
+          <TabPanel><DressCode/></TabPanel>
+          <TabPanel><Faq/></TabPanel>
+          <TabPanel><Registry/></TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
 }
 
-export default App
+export default App;
